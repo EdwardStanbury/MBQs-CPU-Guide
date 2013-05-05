e754bfade44a50d73c2009196ab0e6f91ea1530f
mbq <mbq@mbq-p7-1534.(none)>
2013-05-05 02:48:17 -0700

    Make it mine

---
+++ MBQs-CPU-Guide-+/gen/com/mbq/menudrawer/mbqscpuguide/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.mbq.menudrawer.mbqscpuguide;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ MBQs-CPU-Guide-+/src/com/mbq/menudrawer/mbqscpuguide/ActionBarOverlaySample.java
@@ -0,0 +1,13 @@
+package com.mbq.menudrawer.mbqscpuguide;
+
+import android.os.Bundle;
+import android.view.Window;
+
+public class ActionBarOverlaySample extends WindowSample {
+
+    @Override
+    public void onCreate(Bundle inState) {
+        requestWindowFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        super.onCreate(inState);
+    }
+}
